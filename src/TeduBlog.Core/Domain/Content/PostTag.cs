dfcc26d14e250880be16d16b9a5dd8533b7f4d6e
HoanExV <trc_hoan.nk@extremevn.com.vn>
2025-08-07 16:38:04 +0700

    update code

---
+++ src/TeduBlog.Core/Domain/Content/PostTag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TeduBlog.Core.Domain.Content
+{
+    [Table("PostTags")]
+    [PrimaryKey(nameof(PostId), nameof(TagId))]
+    public class PostTag
+    {
+        public Guid PostId { get; set; }
+        public Guid TagId { get; set; }
+    }
+}
---
+++ src/TeduBlog.Data/TeduBlogContext.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TeduBlog.Core.Domain.Content;
+using TeduBlog.Core.Domain.Identity;
+
+namespace TeduBlog.Data
+{
+    public class TeduBlogContext : IdentityDbContext<AppUser, AppRole, Guid>
+    {
+        public TeduBlogContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostCategory> PostCategorys { get; set; }
+        public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<PostActivityLog> PostActivityLogs { get; set; }
+        public DbSet<Series> Series { get; set; }
+        public DbSet<PostInSeries> PostInSeries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<IdentityUserClaim<Guid>>().ToTable("AppUserClaims").HasKey(x => x.Id);
+
+            builder.Entity<IdentityRoleClaim<Guid>>().ToTable("AppRoleClaims").HasKey(x => x.Id);
+
+            builder.Entity<IdentityUserLogin<Guid>>().ToTable("AppUserLogins").HasKey(x => x.UserId);
+
+            builder.Entity<IdentityUserRole<Guid>>().ToTable("AppUserRoles").HasKey(x => new { x.RoleId, x.UserId });
+
+            builder.Entity<IdentityUserToken<Guid>>().ToTable("AppUserTokens").HasKey(x => new { x.UserId });
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker.Entries().Where(x => x.State == EntityState.Added || x.State == EntityState.Modified);
+
+            foreach (var entityEntry in entries)
+            {
+                var dateCreatedProp = entityEntry.Entity.GetType().GetProperty("DateCreated");
+                if (entityEntry.State == EntityState.Added && dateCreatedProp != null)
+                {
+                    dateCreatedProp.SetValue(entityEntry.Entity, DateTime.Now, null);
+                }
+
+                var dateModifiedProp = entityEntry.Entity.GetType().GetProperty("DateModified");
+                if (entityEntry.State == EntityState.Modified && dateModifiedProp != null)
+                {
+                    dateModifiedProp.SetValue(entityEntry.Entity, DateTime.Now, null);
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
